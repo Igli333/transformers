4503fce32467cd0c03d9821fb37d72ccf544361f
Lameya Islam <lameya@Lameyas-MacBook-Air.local>
2026-01-11 21:45:45 +0100

    Update llama-moe

--- src/transformers/models/llama_moe/__init__.py
+++ src/transformers/models/llama_moe/__init__.py
@@ -1,5 +1,5 @@
-from .configuration_llama_moe import LlamaMoEConfig  # noqa: F401
-from .modeling_llama_moe import (  # noqa: F401
+from .configuration_llama_moe import LlamaMoEConfig  #noqa: F401
+from .modeling_llama_moe import (  #noqa: F401
     BaseMoEModelOutputWithPast,
     LlamaMoEDecoderLayer,
     LlamaMoEForCausalLM,
